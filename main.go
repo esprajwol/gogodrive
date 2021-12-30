bcebf2e22f23a99c8e12bc4b2d8cda9b8b619e2c
esprajwol <esprajwol@gmail.com>
2021-12-30 17:07:11 +0545

    chore: new service use

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
+	option "google.golang.org/api/option"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
+/* // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
@@ -27,14 +26,14 @@ func getClient(config *oauth2.Config) *http.Client {
 		saveToken(tokFile, tok)
 	}
 	return config.Client(context.Background(), tok)
-}
+} */
 
 // Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+/* func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
-
+ß
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
 		log.Fatalf("Unable to read authorization code %v", err)
@@ -45,7 +44,7 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 		log.Fatalf("Unable to retrieve token from web %v", err)
 	}
 	return tok
-}
+} */
 
 // Retrieves a token from a local file.
 func tokenFromFile(file string) (*oauth2.Token, error) {
@@ -56,10 +55,13 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	defer f.Close()
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
+	if err != nil {
+		log.Fatalf("Unable %v", err)
+	}
 	return tok, err
 }
 
-// Saves a token to a file path.
+/* // Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
@@ -67,9 +69,13 @@ func saveToken(path string, token *oauth2.Token) {
 		log.Fatalf("Unable to cache oauth token: %v", err)
 	}
 	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
+	err = json.NewEncoder(f).Encode(token)
+	if err != nil {
+		log.Fatalf("Unable %v", err)
+	}
+} */
 
+/*
 func getToken() {
 	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
@@ -161,6 +167,7 @@ func createFile(service *drive.Service, name string, mimeType string, content io
 
 	return file, nil
 }
+*/
 
 func main() {
 
@@ -168,15 +175,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
+	ctx := context.Background()
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveMetadataScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
 
-	srv, err := drive.New(client)
+	//client := getClient(config)
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	srv, err := drive.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, tok)))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
